3ffb2dbf7aa8ee5d7d8666dc6aff6cf88237b327
amit1995boop <144105948+amit1995boop@users.noreply.github.com>
2025-12-19 19:13:53 +0800

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,17 @@
+#Task 1
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("addition is", add)
+print("subtraction is", sub)
+print("multiplication is", mul)
+print("division is", div)
+
+#Task 2
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+concatenation=first_name+" "+last_name
+print("Hello",concatenation,"hope you have great exp learning python")
